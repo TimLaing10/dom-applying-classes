c2b17651a8b26d66f096d543a948b356a3aafc8c
Tim Laing <timlaing@Tims-MacBook-Pro-2.local>
2017-01-26 13:29:25 +1300

    added files

--- classes.js
+++ classes.js
@@ -1,13 +1,16 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+
   // Your turn! Create a new function called `two`, then call it from here.
+  two()
+  three ()
+  makeVisible()
 }
 
 function one () {
@@ -20,6 +23,22 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
+function two (){
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+}
+
 // CREATE FUNCTION three HERE
 
+function three (){
+  var three = document.getElementById('three')
+  three.classList.add('purple')
+}
+
 // CREATE FUNCTION makeVisible HERE
+
+function makeVisible(){
+  var invisible = document.getElementsByClassName('invisible')
+  invisible[0].classList.add('visible');
+}
